1e38faaa0a5db2a9142c01202873fb1032a24ff8
E. A. Graham Jr. <10370165+EAGrahamJr@users.noreply.github.com>
2025-01-05 12:26:08 -0800

    Update build only - no changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,27 @@
-buildscript {
-    dependencies {
-        classpath("crackers.buildstuff:crackers-gradle-plugins:1.0.1")
-    }
-}
+/*
+ * Copyright 2022-2025 by E. A. Graham, Jr.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
 
 plugins {
     kotlin("jvm") version "1.9.0"
     `java-library`
     idea
     id("org.jmailen.kotlinter") version "3.12.0"
-    id("library-publish") version "1.0.1"
     id("org.jetbrains.dokka") version "1.8.10"
     id("net.thauvin.erik.gradle.semver") version "1.0.4"
+    id("crackers.buildstuff.crackers-gradle-plugins") version "1.1.0"
 }
 
 group = "crackers.automation"
